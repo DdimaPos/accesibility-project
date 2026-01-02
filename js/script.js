f0b441b252c205e1e3331efd99a6cee6a9065de2
DdimaPos <postoroncadumitru@gmail.com>
2026-01-02 20:23:02 +0200

    feat: created all the sections of the website

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 class NavigationView {
   attachDropdownMenuHandler() {
     const navMenu = document.querySelector(".nav__menu");
+    const menuButton = navMenu.querySelector("button");
 
     navMenu.addEventListener("click", (ev) => {
       ev.stopPropagation();
+      const expanded = menuButton.getAttribute('aria-expanded') === 'true';
       navMenu.classList.toggle("active");
+      menuButton.setAttribute('aria-expanded', !expanded)
     });
 
     document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") navMenu.classList.remove("active");
+      if (e.key === "Escape") {n
+        navMenu.classList.remove("active");
+        menuButton.setAttribute("aria-expanded", "false");
+      }
     });
 
     document.addEventListener("click", (e) => {
       if (!navMenu.contains(e.target)) {
         navMenu.classList.remove("active");
+        menuButton.setAttribute("aria-expanded", "false");
       }
     });
   }
